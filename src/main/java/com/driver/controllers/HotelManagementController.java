0764340ecf511eb0028518cafd8965eebe0207cd
Your Name <avinashpsahukar@gmail.com>
2023-09-04 20:53:46 +0530

    fist attem7

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -48,9 +48,10 @@ public class HotelManagementController {
 
         //You need to add a User Object to the database
         //Assume that user will always be a valid user and return the aadharCardNo of the user
+
+        if (user == null) return null ;
         return hotelManagementService.addUser(user) ;
 
-//       return null;
     }
 
     @GetMapping("/get-hotel-with-most-facilities")
@@ -59,9 +60,10 @@ public class HotelManagementController {
         //Out of all the hotels we have added so far, we need to find the hotelName with most no of facilities
         //Incase there is a tie return the lexicographically smaller hotelName
         //Incase there is not even a single hotel with atleast 1 facility return "" (empty string)
-        return hotelManagementService.getHotelWithMostFacilities() ;
+
 
 //        return null;
+        return hotelManagementService.getHotelWithMostFacilities() ;
     }
 
     @PostMapping("/book-a-room")
@@ -73,6 +75,7 @@ public class HotelManagementController {
         //Calculate the total amount paid by the person based on no. of rooms booked and price of the room per night.
         //If there arent enough rooms available in the hotel that we are trying to book return -1 
         //in other case return total amount paid
+        if (booking == null) return 0 ;
         return hotelManagementService.bookARoom(booking) ;
         
 //        return 0;
@@ -82,6 +85,7 @@ public class HotelManagementController {
     public int getBookings(@PathVariable("aadharCard")Integer aadharCard)
     {
         //In this function return the bookings done by a person
+        if (aadharCard == null) return 0 ;
         return hotelManagementService.getBookings(aadharCard) ;
 //        return 0;
     }
@@ -93,6 +97,7 @@ public class HotelManagementController {
         //If the hotel is already having that facility ignore that facility otherwise add that facility in the hotelDb
         //return the final updated List of facilities and also update that in your hotelDb
         //Note that newFacilities can also have duplicate facilities possible
+        if (newFacilities == null || hotelName == null) return null ;
         return hotelManagementService.updateFacilities(newFacilities,hotelName) ;
 //        return null;
     }
